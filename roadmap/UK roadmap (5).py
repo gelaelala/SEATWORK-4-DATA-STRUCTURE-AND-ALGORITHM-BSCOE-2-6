82fcd315ba24de32a82a9d2e3b72f1156ef8cc78
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 03:40:11 +0800

    added a loop where it shows the neighbor cities of london along with its distances

--- roadmap/UK roadmap (5).py
+++ roadmap/UK roadmap (5).py
@@ -2,3 +2,5 @@ from graph import City, load_graph
 
 
 nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+for neighbor, weights in graph[nodes["london"]].items():
+    print (weights ["distance"], neighbor.name)
