a83a5ff4d6d6c870ee51539b8967f3fe5b9b3f7f
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 04:21:09 +0800

    added variables

--- roadmap/UK roadmap (8).py
+++ roadmap/UK roadmap (8).py
@@ -6,5 +6,9 @@ from graph import (
 )
 
 def is_twentieth_century (city):
-    return city.year and 1901 <= year <= 2000
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+city = bfs (graph, nodes ["edinburgh"], is_twentieth_century)
+print(city.name )
 
