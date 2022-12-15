96524b45184edc71de480825f3b9576baf584a4e
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 03:54:58 +0800

    added another defined function

--- roadmap/UK roadmap (7).py
+++ roadmap/UK roadmap (7).py
@@ -4,6 +4,11 @@ from graph import City, load_graph
 def is_twentieth_century (year):
     return year and 1901 <= year <= 2000
 
+def order (neighbors):
+    def by_latitude (city):
+        return city.latitude
+    return iter (sorted (neighbors, key = by_latitude, reverse = True))
+
 nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
 for node in nx.bfs_tree (graph, nodes ["edinburgh"]):
     print ("📍", node.name)
