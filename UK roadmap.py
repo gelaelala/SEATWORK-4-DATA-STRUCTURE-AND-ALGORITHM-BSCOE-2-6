044ba58ac373e1cc84813bcb2b6af454a2d0056e
unknown <angelaescariocorpuz@gmail.com>
2022-12-15 22:23:28 +0800

    added sort_by and by_distance functions

--- UK roadmap.py
+++ UK roadmap.py
@@ -5,5 +5,11 @@ nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 print(nodes ["london"])
 print (graph)
 
+def sort_by (neighbors, strategy):
+    return sorted (neighbors.items (), key = lambda item: strategy (item[1]))
+
+def by_distance (weights):
+    return float (weights ["distance"])
+
 for neighbor, weights in sort_by (graph [nodes["london"]], by_distance):
     print (f"{weights ['distance']:>3} miles, {neighbor.name}")
