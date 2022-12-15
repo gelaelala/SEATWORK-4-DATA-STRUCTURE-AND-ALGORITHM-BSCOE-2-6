958854448baaafaf50eef1c9d1786b96947bc0e2
unknown <angelaescariocorpuz@gmail.com>
2022-12-15 22:12:05 +0800

    imported graph.py into the program and added variables

--- UK roadmap.py
+++ UK roadmap.py
@@ -1,4 +1,3 @@
-import networkx as nx
+from graph import City, load_graph
 
-graph = nx.nx_agraph.read_dot("roadmap.dot")
-print(graph.nodes["london"])
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
