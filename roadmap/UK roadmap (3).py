766911659efa4b9fe9c15fe1a56f030a13e4b457
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 03:29:05 +0800

    edited variable and added print for the dictionary of london and for the graph as well

--- roadmap/UK roadmap (3).py
+++ roadmap/UK roadmap (3).py
@@ -1,3 +1,7 @@
 from graph import City, load_graph
 
-nodes, graph = load_graph ("roadmap/roadmap.dot, City.from_dict")
+nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+
+print (nodes ["london"])
+
+print (graph)
