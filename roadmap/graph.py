dc02fa10827e40d64df8db0e767eca8e6385b100
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 21:35:22 +0800

    added code under depth_first_traverse function and imported stack from queues program as well

--- roadmap/graph.py
+++ roadmap/graph.py
@@ -1,6 +1,6 @@
 from typing import NamedTuple
 import networkx as nx
-from queues import Queue
+from queues import Queue, Stack
 from collections import deque
 
 
@@ -84,3 +84,14 @@ def connected (graph, source, destination):
     return shortest_path (graph, source, destination) is not None
 
 def depth_first_traverse (graph, source, order_by = None):
+    stack = Stack (source)
+    visited = set ()
+    while stack:
+        if (node := stack.dequeue ()) not in visited:
+            yield node
+            visited.add (node)
+            neighbors = list (graph.neighbors (node))
+            if order_by:
+                neighbors.sort (key = order_by)
+            for neighbor in reversed (neighbors):
+                stack.enqueue (neighbor)
