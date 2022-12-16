3b6cc6f411cfc97f3adbec66b566c18829ad9ca3
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 22:42:28 +0800

    added main and parse_args function

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -7,3 +7,14 @@ QUEUE_TYPES = {
     "heap": PriorityQueue
 }
 
+def main (args):
+    buffer = QUEUE_TYPES [args.queue]()
+
+def parse_args ():
+    parser = argparse.ArgumentParser ()
+    parser.add_argument ("-q", "--queue", choices = QUEUE_TYPES, default = "fifo")
+    parser.add_argument ("-p", "--producers", type = int, default = 3)
+    parser.add_argument ("-c", "--consumers", type = int, default = 2)
+    parser.add_argument ("-ps", "--producder-speed", type = int, default = 1)
+    parser.add_argument ("-cs", "--consumer-speed", type = int, default = 1) 
+
