462bbfd0feefc94c0bfd88d9b301953b4b3d0b11
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 02:42:35 +0800

    create display function

--- async_queues.py
+++ async_queues.py
@@ -17,3 +17,10 @@ def parse_args ():
     parser.add_argument("d", "--max-depth", type = int, default = 2)
     parser.add_argument("w", "--num-workers", type = int, default = 3)
     return parser.parse_args()
+
+def display (links):
+    for url, count in links.most_common():
+        print (f"{count :> 3} {url}")
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))
