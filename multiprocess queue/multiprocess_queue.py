e6528f78e17c118004c538b88ec128a806dd165a
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 03:48:57 +0800

    added __len__ and __getitem__ function

--- multiprocess queue/multiprocess_queue.py
+++ multiprocess queue/multiprocess_queue.py
@@ -7,6 +7,19 @@ class Combinations:
     def __init__(self, alphabet, length):
         self.alphabet = alphabet
         self.length = length
+    
+    def __len__(self):
+        return len(self.alphabet) ** self.length
+    
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError
+        return "".join(
+            self.alphabet[
+                (index // len(self.alphabet) ** i) % len(self.alphabet)
+            ]
+            for i in reversed(range(self.length))
+        )
 
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
     for length in range(1, max_length + 1):
