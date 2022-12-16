403fb8dcadacb12ae214c7d420db7f17546c03ce
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 03:34:18 +0800

    create chunk_indices function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -16,5 +16,9 @@ def main():
     text = reverse_md5("a9d1cbf71942327e98b40cf5ef38a960")
     print (f"{text} (found in {time.perf_counter() - t1:.1f}s)")
 
+def chuck_indices(length, num_chucks):
+    start = 0
+    
+
 if __name__ == "__main__":
     main()
