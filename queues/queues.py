13209e1473b592c74daa084a5b8fa959cda8f64a
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:39:28 +0800

    modifications under Queue and PriorityQueue class

--- queues/queues.py
+++ queues/queues.py
@@ -10,16 +10,9 @@ class IterableMixin:
         while len (self) > 0:
             yield self.dequeue ()
 
-class Queue:
+class Queue (IterableMixin):
     def __init__ (self, *elements):
         self._elements = deque (elements)
-
-    def __len__ (self):
-        return len (self._elements)
-
-    def __iter__ (self):
-        while len (self) > 0:
-            yield self.dequeue ()
 
     def enqueue (self, element):
         self._elements.append (element)
@@ -31,7 +24,7 @@ class Stack (Queue):
     def dequeue (self):
         return self._elements.pop ()
 
-class PriorityQueue:
+class PriorityQueue (IterableMixin):
     def __init__ (self):
         self._elements = []
         self._counter = count ()
