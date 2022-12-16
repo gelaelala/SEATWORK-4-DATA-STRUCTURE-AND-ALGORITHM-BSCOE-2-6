4068b5940f3eb22dd598fe6088c7569698e19816
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:56:22 +0800

    changed directory for roadmap.dot

--- roadmap/UK roadmap (7).py
+++ roadmap/UK roadmap (7).py
@@ -9,7 +9,7 @@ def order (neighbors):
         return city.latitude
     return iter (sorted (neighbors, key = by_latitude, reverse = True))
 
-nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 for node in nx.bfs_tree (graph, nodes ["edinburgh"], sort_neighbors = order):
     print ("📍", node.name)
     if is_twentieth_century (node.year):
