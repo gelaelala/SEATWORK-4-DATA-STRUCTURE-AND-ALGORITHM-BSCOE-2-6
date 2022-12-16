cab6c8d0b798f733a55371ab19294c9ba1e04ab8
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 19:17:33 +0800

    created loop where it prints out the shortest paths

--- roadmap/UK roadmap (9).py
+++ roadmap/UK roadmap (9).py
@@ -5,3 +5,6 @@ nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 
 city1 = nodes ["aberdeen"]
 city2 = nodes ["perth"]
+
+for i, path in enumerate (nx.all_shortest_paths (graph, city1, city2), 1):
+    print (f"{i}.", " → ".join (city.name for city in path))
