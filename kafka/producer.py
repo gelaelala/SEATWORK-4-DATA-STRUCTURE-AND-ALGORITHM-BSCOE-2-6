50732e2057cb8f62c8c9d18c00e3329531ae54bf
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 05:22:22 +0800

    create while loop

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,3 +1,9 @@
 from kafka3 import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers="localhost:9092")
+while True:
+    message = input("Message: ")
+    producer.send(
+        topic="datascience",
+        value=message.encode("utf-8"),
+    )
