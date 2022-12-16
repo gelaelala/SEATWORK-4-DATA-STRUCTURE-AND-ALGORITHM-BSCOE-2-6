7d9f05880c37e289d9b9b69b5e1892dfc985c899
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:57:46 +0800

    duplicate file from queues folder in order for the other programs to work

---
+++ roadmap/queues.py
@@ -0,0 +1,37 @@
+from collections import deque
+from heapq import heappop, heappush
+from itertools import count
+
+class IterableMixin:
+    def __len__ (self):
+        return len (self._elements)
+    
+    def __iter__ (self):
+        while len (self) > 0:
+            yield self.dequeue ()
+
+class Queue (IterableMixin):
+    def __init__ (self, *elements):
+        self._elements = deque (elements)
+
+    def enqueue (self, element):
+        self._elements.append (element)
+
+    def dequeue (self):
+        return self._elements.popleft ()
+
+class Stack (Queue):
+    def dequeue (self):
+        return self._elements.pop ()
+
+class PriorityQueue (IterableMixin):
+    def __init__ (self):
+        self._elements = []
+        self._counter = count ()
+
+    def enqueue_with_priority (self, priority, value):
+        element = (-priority, next (self._counter), value)
+        heappush (self._elements, element)
+
+    def dequeue (self):
+        return heappop (self._elements) [-1]
