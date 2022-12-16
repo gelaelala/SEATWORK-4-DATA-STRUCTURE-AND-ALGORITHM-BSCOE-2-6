91cbc9d0c79daba9862db4405796e3a7c3303490
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 19:29:32 +0800

    added more code under retrace function

--- roadmap/graph.py
+++ roadmap/graph.py
@@ -76,3 +76,6 @@ def retrace (previous, source, destination):
         current = previous.get (current)
         if current is None:
             return None
+    
+    path.appendleft (source)
+    return list (path)
