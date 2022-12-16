c1bc17bda5c08f3ded992b4a16a8022274e24e98
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 04:53:54 +0800

    added while loop

--- rabbitmq/producer.py
+++ rabbitmq/producer.py
@@ -1,3 +1,9 @@
 import pika
 
 QUEUE_NAME = "mailbox"
+
+with pika.BlockingConnection() as connection:
+    channel = connection.channel()
+    channel.queue_decalre(queue=QUEUE_NAME)
+    while True:
+        
