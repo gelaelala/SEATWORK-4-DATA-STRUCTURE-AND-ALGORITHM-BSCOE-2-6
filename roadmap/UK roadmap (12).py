5e61198f8a4429e6eeef6a7d4bbc3fb3e90e2069
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 22:25:45 +0800

    added distance function and for loop

--- roadmap/UK roadmap (12).py
+++ roadmap/UK roadmap (12).py
@@ -1,7 +1,13 @@
 import networkx as nx
 from graph import City, load_graph, dijkstra_shortest_path
 
-nodes, graph = load_graph ("raodmap.dot", City.from_dict)
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 
 city1 = nodes ["london"]
 city2 = nodes ["edinburgh"]
+
+def distance (weights):
+    return float (weights ["distance"])
+
+for city in dijkstra_shortest_path (graph, city1, city2, distance):
+    print (city.name)
