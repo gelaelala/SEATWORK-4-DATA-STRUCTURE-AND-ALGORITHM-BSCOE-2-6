cf6c13e6a2bfbd1d4f6cc08a218500ba257a19eb
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 12:38:50 +0800

    improved the __init__ function under class Queue

--- queues/queues.py
+++ queues/queues.py
@@ -1,8 +1,8 @@
 from collections import deque
 
 class Queue:
-    def __init__ (self):
-        self._elements = deque ()
+    def __init__ (self, *elements):
+        self._elements = deque (elements)
     def enqueue (self, element):
         self._elements.append (element)
     def dequeue (self):
