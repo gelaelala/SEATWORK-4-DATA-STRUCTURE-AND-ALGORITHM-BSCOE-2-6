9f8aa778583c054e6dcc587d450aa0e126b4a3e7
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:52:32 +0800

    changed directory for roadmap.py

--- roadmap/UK roadmap (4).py
+++ roadmap/UK roadmap (4).py
@@ -1,6 +1,6 @@
 from graph import City, load_graph
 
 
-nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 for neighbor in graph.neighbors(nodes["london"]):
     print (neighbor.name)
