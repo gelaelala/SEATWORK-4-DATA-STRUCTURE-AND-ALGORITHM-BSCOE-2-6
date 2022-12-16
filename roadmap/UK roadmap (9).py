db5165c33335cced5f504d27e30a9763e7e44d10
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 21:13:07 +0800

    printed out the natural order of neighbors from the DOT file

--- roadmap/UK roadmap (9).py
+++ roadmap/UK roadmap (9).py
@@ -1,5 +1,5 @@
 import networkx as nx
-from graph import City, load_graph
+from graph import City, load_graph, shortest_path
 
 nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 
@@ -8,3 +8,6 @@ city2 = nodes ["perth"]
 
 for i, path in enumerate (nx.all_shortest_paths (graph, city1, city2), 1):
     print (f"{i}.", " → ".join (city.name for city in path))
+
+
+print (" → ".join (city.name for city in shortest_path (graph, city1, city2)))
