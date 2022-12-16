e147c7627fada96bc216a6e7aff0491d43824076
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:19:38 +0800

    tested if it prints out from highest to lowest

--- queues/priorityq.py
+++ queues/priorityq.py
@@ -11,3 +11,9 @@ messages.enqueue_with_priority (CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority (IMPORTANT, "Hazard lights turned on")
 
 print (messages.dequeue())
+
+print (messages.dequeue())
+
+print (messages.dequeue())
+
+print (messages.dequeue())
