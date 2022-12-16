bdebba285b4a24643368ab8e13a09c0524025fac
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 05:10:13 +0800

    added with statement

--- redis/subscriber.py
+++ redis/subscriber.py
@@ -1,1 +1,9 @@
 import redis
+
+with redis.Redis() as client:
+    pubsub = client.pubsub()
+    pubsub.subscribe("chatroom")
+    for message in pubsub.listen():
+        if message["type"] == "message":
+            body = message["data"].decode("utf-8")
+            print (f"Got message: {body}")
