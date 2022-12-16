82d5e3d25a331b696111875743859313cad92666
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:01:54 +0800

    imported heappop function from heapq module

--- queues/heap data.py
+++ queues/heap data.py
@@ -1,8 +1,13 @@
 from heapq import heappush
+from heapq import heappop
 
 fruits = []
 heappush (fruits, "orange")
 heappush (fruits, "apple")
 heappush (fruits, "banana")
+
+print (fruits)
+
+print(heappop (fruits))
 
 print (fruits)
