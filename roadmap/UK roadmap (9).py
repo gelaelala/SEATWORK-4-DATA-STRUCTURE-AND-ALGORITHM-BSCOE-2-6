04c3f94b288d08e2bb49724898885cf428b59517
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 21:21:06 +0800

    imported connected function into the program

--- roadmap/UK roadmap (9).py
+++ roadmap/UK roadmap (9).py
@@ -1,5 +1,5 @@
 import networkx as nx
-from graph import City, load_graph, shortest_path
+from graph import City, load_graph, shortest_path, connected
 
 nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 
