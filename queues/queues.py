cbfe971ee85a81a6a0af86b19b5b76ef34557b51
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:33:42 +0800

    improved PriorityQueue class

--- queues/queues.py
+++ queues/queues.py
@@ -26,9 +26,11 @@ class Stack (Queue):
 class PriorityQueue:
     def __init__ (self):
         self._elements = []
+        self._counter = count ()
 
     def enqueue_with_priority (self, priority, value):
-        heappush (self._elements, (-priority, value))
+        element = (-priority, next (self._counter), value)
+        heappush (self._elements, element)
 
     def dequeue (self):
-        return heappop (self._elements) [1]
+        return heappop (self._elements) [-1]
