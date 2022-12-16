1d8ce64de57e6dab1e9d26552f263d0b75847b51
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 03:28:42 +0800

    created reverse_md5 function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -2,3 +2,11 @@ import time
 from hashlib import md5
 from itertools import product
 from string import ascii_lowercase
+
+def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
+    for length in range(1, max_length + 1):
+        for combination in product(alphabet, repeat=length):
+            text_bytes = "".join(combination).encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
