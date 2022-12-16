7377013d2545314e8531b85684ebe6b923322910
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 03:31:24 +0800

    create main function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -10,3 +10,11 @@ def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
             hashed = md5(text_bytes).hexdigest()
             if hashed == hash_value:
                 return text_bytes.decode("utf-8")
+
+def main():
+    t1 = time.perf_counter()
+    text = reverse_md5("a9d1cbf71942327e98b40cf5ef38a960")
+    print (f"{text} (found in {time.perf_counter() - t1:.1f}s)")
+
+if __name__ == "__main__":
+    main()
