fdbd012b838fc5cc89a6e614768e15911f469293
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 01:16:01 +0800

    added animate and render function

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -70,7 +70,22 @@ class View:
         self.producers = producers
         self.consumers = consumers
     
-    
+    def animate (self):
+        with Live (
+            self.render (), screen = True, refresh_per_second = 10   
+        ) as live:
+            while True:
+                live.update (self.render())
+
+    def render (self):
+
+        match self.buffer:
+            case PriorityQueue ():
+                title = "Priority Queue"
+                products = map (str, reversed (list(self.buffer.queue)))
+            case LifoQueue ():
+                title = "Stack"
+                products = list (self.buffer.queue)
 
 def main (args):
     buffer = QUEUE_TYPES [args.queue]()
