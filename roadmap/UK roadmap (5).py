ea40f80dcc727c181d051addc9c72510f9f7228a
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:54:00 +0800

    changed directory for roadmap.py

--- roadmap/UK roadmap (5).py
+++ roadmap/UK roadmap (5).py
@@ -1,6 +1,6 @@
 from graph import City, load_graph
 
 
-nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 for neighbor, weights in graph[nodes["london"]].items():
     print (weights ["distance"], neighbor.name)
