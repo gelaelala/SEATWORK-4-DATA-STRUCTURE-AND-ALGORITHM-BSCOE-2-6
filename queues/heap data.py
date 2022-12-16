ef67f539593ae8cfa9d2b35225efe35052568d84
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:04:56 +0800

    tested out Python's tuple comparison

--- queues/heap data.py
+++ queues/heap data.py
@@ -11,3 +11,10 @@ print (fruits)
 print(heappop (fruits))
 
 print (fruits)
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+print (person1 < person2)
+print (person2 < person3)
