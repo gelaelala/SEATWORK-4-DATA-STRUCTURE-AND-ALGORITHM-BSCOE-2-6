7ba15c91dfc8f619c37b0a48701b75c3a6d07596
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 02:40:46 +0800

    create parse_args function

--- async_queues.py
+++ async_queues.py
@@ -11,3 +11,9 @@ async def main (args):
     finally:
         await session.close()
 
+def parse_args ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("d", "--max-depth", type = int, default = 2)
+    parser.add_argument("w", "--num-workers", type = int, default = 3)
+    return parser.parse_args()
