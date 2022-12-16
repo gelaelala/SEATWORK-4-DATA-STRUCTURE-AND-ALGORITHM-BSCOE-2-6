f21e3c40c43fb76c2f9d4355a4e6881239be8178
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 21:28:38 +0800

    created variables and added another function

--- roadmap/UK roadmap (10).py
+++ roadmap/UK roadmap (10).py
@@ -3,3 +3,11 @@ from graph import City, load_graph
 
 def is_twentieth_century (year):
     return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
+for node in nx.dfs_tree (graph, nodes ["edinburgh"]):
+    print ("📍 ", node.name)
+    if is_twentieth_century (node.year):
+        print ("Found: ", node.name, node.year)
+else:
+    print ("Not found")
