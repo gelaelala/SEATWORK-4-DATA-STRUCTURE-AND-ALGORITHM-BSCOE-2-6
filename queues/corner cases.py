e832c9d9aef7425953963656c71f2b591ca31456
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:28:54 +0800

    implemented PriorityQueue in program and adding priority levels

--- queues/corner cases.py
+++ queues/corner cases.py
@@ -5,5 +5,15 @@ from dataclasses import dataclass
 class Message:
     event: str
 
+CRITICAL = 3 
+IMPORTANT = 2
+NEUTRAL = 1   
+
 wipers = Message ("Windshield wipers turned on")
 hazard_lights = Message ("Hazard lights turned on")
+
+print (wipers < hazard_lights)
+
+messages = PriorityQueue ()
+messages.enqueue_with_priority (CRITICAL, wipers)
+messages.enqueue_with_priority (IMPORTANT, hazard_lights)
