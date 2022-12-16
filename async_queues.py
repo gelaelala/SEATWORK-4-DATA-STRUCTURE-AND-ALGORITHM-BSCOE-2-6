fff0b10f8c30716acf194f13d3fa975753386429
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 02:57:42 +0800

    modified main function

--- async_queues.py
+++ async_queues.py
@@ -15,6 +15,19 @@ async def main (args):
     session = aiohttp.ClientSession()
     try:
         links = Counter()
+        queue = asyncio.Queue()
+        tasks = [
+            asyncio.create_task(
+                worker(
+                    f"Worker-{i + 1}",
+                    session,
+                    queue,
+                    links,
+                    args.max_depth,
+                )
+            )
+            for i in range(args.num_workers)
+        ]
         display (links)
     finally:
         await session.close()
