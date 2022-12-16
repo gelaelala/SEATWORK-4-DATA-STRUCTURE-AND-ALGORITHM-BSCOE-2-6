edf25579de6f7def9cf1e60c7768e299905ed3ad
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:09:53 +0800

    added equeue_with_priority and dequeue function under PriorityQueue class

--- queues/queues.py
+++ queues/queues.py
@@ -25,3 +25,9 @@ class Stack (Queue):
 class PriorityQueue:
     def __init__ (self):
         self._elements = []
+
+    def enqueue_with_priority (self, priority, value):
+        heappush (self._elements,(priority, value) )
+
+    def dequeue (self):
+        return heappop (self._elements)
