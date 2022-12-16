46307759b26b3a3f72b17db1ee9e9a3a20f00028
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 04:55:50 +0800

    added condition under while loop

--- rabbitmq/producer.py
+++ rabbitmq/producer.py
@@ -6,4 +6,9 @@ with pika.BlockingConnection() as connection:
     channel = connection.channel()
     channel.queue_decalre(queue=QUEUE_NAME)
     while True:
-        
+        message = input("Message: ")
+        channel.basic_publish(
+            exchnage = "",
+            routing_key = QUEUE_NAME,
+            body = message.encode("utf-8")
+        )
