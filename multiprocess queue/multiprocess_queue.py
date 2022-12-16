70f01279a5c0b323dd6f3b6b6b538f43cefe5cfc
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 04:00:14 +0800

    added __call__ function under Job class

--- multiprocess queue/multiprocess_queue.py
+++ multiprocess queue/multiprocess_queue.py
@@ -29,6 +29,13 @@ class Job:
     start_index: int
     stop_index: int
 
+    def __call__(self, hash_value):
+        for index in range(self.start_index, self.stop_index):
+            text_bytes = self.combinations[index].encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
+
 class Worker(multiprocessing.Process):
     def __init__(self, queue_in, queue_out, hash_value):
         super().__init__(daemon=True)
