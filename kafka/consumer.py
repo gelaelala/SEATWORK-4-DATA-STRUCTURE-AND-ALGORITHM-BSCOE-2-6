4fd8d99ca7d4d3a76dcd5882e9d4525cd4e1ed42
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 05:25:28 +0800

    added for loop

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,3 +1,6 @@
 from kafka3 import KafkaConsumer
 
 consumer = KafkaConsumer("datascience")
+for record in consumer:
+    message = record.value.decode("utf-8")
+    print (f"Got message: {message}")
