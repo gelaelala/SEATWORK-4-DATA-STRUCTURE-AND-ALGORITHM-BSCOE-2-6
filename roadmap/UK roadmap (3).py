32a9c5edd6f5d5d337d0a8b92be7061b91629663
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:51:23 +0800

    changed directory for roadmap.dot

--- roadmap/UK roadmap (3).py
+++ roadmap/UK roadmap (3).py
@@ -1,6 +1,6 @@
 from graph import City, load_graph
 
-nodes, graph = load_graph ("roadmap/roadmap.dot", City.from_dict)
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
 
 print (nodes ["london"])
 
