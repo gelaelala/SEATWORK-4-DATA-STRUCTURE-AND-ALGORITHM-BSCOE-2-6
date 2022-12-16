ae5649b194d2da050cf1945b28b82358164127b1
unknown <angelaescariocorpuz@gmail.com>
2022-12-17 02:38:39 +0800

    added main function

--- async_queues.py
+++ async_queues.py
@@ -2,3 +2,12 @@ import argparse
 import asyncio
 from collections import Counter
 import aiohttp
+
+async def main (args):
+    session = aiohttp.ClientSession()
+    try:
+        links = Counter()
+        display (links)
+    finally:
+        await session.close()
+
