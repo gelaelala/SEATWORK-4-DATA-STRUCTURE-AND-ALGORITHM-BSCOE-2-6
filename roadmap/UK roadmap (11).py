eb2ff4b9cdccf88aa30dd1979e093c2259801fad
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 21:51:50 +0800

    added function and variables

--- roadmap/UK roadmap (11).py
+++ roadmap/UK roadmap (11).py
@@ -5,3 +5,9 @@ from graph import (
     depth_first_search as dfs,
 )
 
+def is_twentieth_century (city):
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph ("roadmap.dot", City.from_dict)
+city = dfs (graph, nodes ["edinburgh"], is_twentieth_century)
+print (city.name)
