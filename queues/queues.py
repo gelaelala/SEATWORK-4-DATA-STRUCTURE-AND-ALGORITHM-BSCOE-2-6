4bc870acf1f53c586ee0edfecc5a051d510e1618
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 13:36:51 +0800

    added functions under IterableMixin class

--- queues/queues.py
+++ queues/queues.py
@@ -3,7 +3,12 @@ from heapq import heappop, heappush
 from itertools import count
 
 class IterableMixin:
+    def __len__ (self):
+        return len (self._elements)
     
+    def __iter__ (self):
+        while len (self) > 0:
+            yield self.dequeue ()
 
 class Queue:
     def __init__ (self, *elements):
