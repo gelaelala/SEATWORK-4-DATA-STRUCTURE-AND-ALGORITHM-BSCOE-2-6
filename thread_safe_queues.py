d54f16d53afd9bbc9bb5d4854cd4aa669c7e5233
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 22:58:53 +0800

    added simulate_work function

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -49,6 +49,14 @@ class Worker (threading.Thread):
         self.progress = 0
         sleep (randint (1, 3))
 
+    def simulate_work (self):
+        self.working = True
+        self.progress = 0
+        delay = randint (1, 1 + 15 // self.speed)
+        for _ in range (100):
+            sleep (delay / 100)
+            self.progress += 1
+
 def main (args):
     buffer = QUEUE_TYPES [args.queue]()
 
