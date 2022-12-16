160fbdee81f1886a39b1102af7169cafa4079349
unknown <angelaescariocorpuz@gmail.com>
2022-12-16 12:22:46 +0800

    created enqueue and dequeue function

--- queues/queues.py
+++ queues/queues.py
@@ -2,5 +2,9 @@ from collections import deque
 
 class Queue:
     def __init__ (self):
-        self.elements = deque ()
+        self._elements = deque ()
+    def enqueue (self, element):
+        self._elements.append (element)
+    def dequeue (self):
+        return self._elements.popleft ()
 
